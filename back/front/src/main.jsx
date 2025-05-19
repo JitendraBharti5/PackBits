7730a949693c9cb8a802f139431890014a96afe2
JITENDRA  BHARTI <144618949+JitendraBharti5@users.noreply.github.com>
2025-05-19 10:18:19 +0530

    changed to hash route

--- back/front/src/main.jsx
+++ back/front/src/main.jsx
@@ -4,13 +4,17 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 import AuthProvider from './context/AuthProvider.jsx';
+import { HashRouter } from 'react-router-dom';
+
+
+  
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-     <BrowserRouter>
+    <HashRouter>
     <AuthProvider>
         <div>
         <App/>
         </div>
     </AuthProvider>
-    </BrowserRouter>,
+    </HashRouter>,
 );
